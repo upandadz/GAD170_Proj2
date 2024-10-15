eb51fc47ff6beddccc55994acf6603dba65bf46b
SAE1\1033963 <1033963@student.sae.edu.au>
2024-10-15 15:42:23 +1100

    tweaks

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,8 +7,8 @@ public class PlayerMovement : MonoBehaviour
    public float speed = 7.0f;
    public float jumpForce = 600f;
    private float move;
-   
-   
+
+   private Transform playerRotation;
    private Rigidbody2D rb;
     void Start()
     {
@@ -18,6 +18,8 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        playerRotation.rotation = 
+        
         move = Input.GetAxis("Horizontal");
         
         rb.velocity = new Vector2(move * speed, rb.velocity.y);
