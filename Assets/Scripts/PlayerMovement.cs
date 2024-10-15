8018f973e2900b31826180d266e143094f02050c
SAE1\1033963 <1033963@student.sae.edu.au>
2024-10-15 15:34:29 +1100

    initial push

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+   public float speed = 7.0f;
+   public float jumpForce = 600f;
+   private float move;
+   
+   
+   private Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxis("Horizontal");
+        
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jumpForce));
+        }
+    }
+}
