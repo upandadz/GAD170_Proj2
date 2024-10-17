6aa33ae35c48a55db52dc24a73b8537350195fb1
AdamAttardSAE <1033963@student.sae.edu.au>
2024-10-17 22:17:31 +1100

    added prefabs & empty scripts

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
+    public Transform playerLocation;
     // Start is called before the first frame update
     void Start()
     {
