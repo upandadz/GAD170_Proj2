02b51e6316e675e2b2ed930c35fa74a296a1209e
SAE1\1033963 <1033963@student.sae.edu.au>
2024-10-29 13:22:34 +1100

    imported free assets

--- Assets/Scripts/FloorSpawner.cs
+++ Assets/Scripts/FloorSpawner.cs
@@ -10,7 +10,7 @@ public class FloorSpawner : MonoBehaviour
     void Start()
     {
         prefabs = FindObjectOfType<ObstaclePrefabs>();
-        int roll = Random.Range(1, 4);
+        int roll = Random.Range(1, 6);
         // do a random roll to find different prefab walls
         if (roll == 1)
         {
@@ -23,6 +23,14 @@ public class FloorSpawner : MonoBehaviour
         else if (roll == 3)
         {
             floor = prefabs.prefabThree;
+        }
+        else if (roll == 4)
+        {
+            floor = prefabs.prefabFour;
+        }
+        else if (roll == 5)
+        {
+            floor = prefabs.prefabFive;
         }
     }
     
